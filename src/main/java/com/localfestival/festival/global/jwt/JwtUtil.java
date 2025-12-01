183b2851e0e459e797c2fde51204f35f1602483e
aneunwook <dmsdnr0720@naver.com>
2025-12-01 16:14:35 +0900

    refactor : accessToken 시간 변경(30분) (#26)

--- src/main/java/com/localfestival/festival/global/jwt/JwtUtil.java
+++ src/main/java/com/localfestival/festival/global/jwt/JwtUtil.java
@@ -23,7 +23,7 @@ public class JwtUtil {
     private final SecretKey secretKey;
 
     private static final String BEARER_PREFIX = "Bearer ";
-    private static final long ACCESS_TOKEN_TIME = 1 * 60 * 1000; // jwt 토큰 시간
+    private static final long ACCESS_TOKEN_TIME = 30 * 60 * 1000; // jwt 토큰 시간 (30분)
     private static final long REFRESH_TOKEN_TIME = 7 * 24 * 60 * 60 * 1000L; // 7일
 
     /**
