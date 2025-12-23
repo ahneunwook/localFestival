5afca00a67fdcffe4e312954fa7dd12da2e553d5
aneunwook <dmsdnr0720@naver.com>
2025-12-23 14:30:02 +0900

    refactor : Cors 수정

--- src/main/java/com/localfestival/festival/global/config/SecurityConfig.java
+++ src/main/java/com/localfestival/festival/global/config/SecurityConfig.java
@@ -86,6 +86,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:5173");
+        configuration.addAllowedOrigin("https://local-festival.vercel.app");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
