2c9075988630b1523e37a8ce763e2bc7673d0941
aneunwook <dmsdnr0720@naver.com>
2025-12-29 16:35:30 +0900

    feat : s3를 이용하여 이미지 저장

--- src/test/java/com/localfestival/festival/LocalFestivalApplicationTests.java
+++ src/test/java/com/localfestival/festival/LocalFestivalApplicationTests.java
@@ -1,11 +1,13 @@
 package com.localfestival.festival;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("local")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class LocalFestivalApplicationTests {
 
 	@Test
